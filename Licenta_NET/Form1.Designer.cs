4413ae0794aa2547945104558eb601937b03d321
Radu Crăciun <radu.craciun420@gmail.com>
2024-03-15 14:00:51 +0200

    Added a Win32 App to simulate touch injection with pressure
    It seems to work quite well for drawing in photoshop, simulating pen pressure. Warning: you need to manually enable Windows Ink input in Photoshop.

---
+++ Licenta_NET/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Licenta
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            absoluteButton = new Button();
+            touchpadButton = new Button();
+            richTextData = new RichTextBox();
+            SuspendLayout();
+            // 
+            // absoluteButton
+            // 
+            absoluteButton.Location = new Point(10, 12);
+            absoluteButton.Name = "absoluteButton";
+            absoluteButton.Size = new Size(75, 23);
+            absoluteButton.TabIndex = 0;
+            absoluteButton.Text = "Absolute";
+            absoluteButton.UseVisualStyleBackColor = true;
+            absoluteButton.Click += absoluteButton_Click;
+            // 
+            // touchpadButton
+            // 
+            touchpadButton.Location = new Point(91, 12);
+            touchpadButton.Name = "touchpadButton";
+            touchpadButton.Size = new Size(75, 23);
+            touchpadButton.TabIndex = 1;
+            touchpadButton.Text = "Touchpad";
+            touchpadButton.UseVisualStyleBackColor = true;
+            touchpadButton.Click += touchpadButton_Click;
+            // 
+            // richTextData
+            // 
+            richTextData.Location = new Point(10, 59);
+            richTextData.Name = "richTextData";
+            richTextData.Size = new Size(276, 294);
+            richTextData.TabIndex = 2;
+            richTextData.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(321, 409);
+            Controls.Add(richTextData);
+            Controls.Add(touchpadButton);
+            Controls.Add(absoluteButton);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button absoluteButton;
+        private Button touchpadButton;
+        private RichTextBox richTextData;
+    }
+}
---
+++ Licenta_Win32/Licenta_Win32.cpp
@@ -0,0 +1,120 @@
+// Licenta_Win32.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <windows.h>
+#include <mcp2221_dll_um.h>
+
+
+POINTER_TYPE_INFO pointerTypeInfo;
+POINTER_PEN_INFO penInfo;
+POINTER_INFO pointerInfo;
+POINT coords;
+
+void updatePen(int x, int y, int pressure) {
+    const int maxPressure = 1 << 12;
+    const int pressureOffset = 1000;
+    int calcPressure = (pressure - pressureOffset) * 1024 / maxPressure;
+    if (calcPressure > 1023) pressure = 1023;
+    if (calcPressure < 0) pressure = 0; 
+    coords.x = x;
+    coords.y = y;
+    pointerInfo.ptPixelLocation = coords;
+    penInfo.pointerInfo = pointerInfo;
+    penInfo.pressure = calcPressure;
+    pointerTypeInfo.penInfo = penInfo; //some stuff might be redundant
+}
+
+void updatePointerFlag(int pointerFlags) {
+    pointerInfo.pointerFlags = POINTER_FLAG_INCONTACT | POINTER_FLAG_PRIMARY;
+    penInfo.pointerInfo = pointerInfo;
+    pointerTypeInfo.penInfo = penInfo; //some stuff may be redundant
+}
+
+int main()
+{   
+    byte i2cPosData[5];
+    byte i2cPressData[2];
+
+    const int VID = 0x4D8;
+    const int PID = 0xDD; //default MCP2221 vals
+    const int I2CSpeed = 100000; //seems stable at this speed
+    const byte AR1000Address = 0x4D; //7 bit address
+    const int NO_POS_BYTES = 5; //AR1000 datasheet, 5 bytes for pos
+
+    const int NO_PRESS_BYTES = 2;
+    const byte FMAAddress = 0x28; //7 bit, Honeywell pressure sensor FMAMSDXX005WC2C3
+
+
+    const int xMax = 1 << 12;
+    const int yMax = 1 << 12;
+    const float edgePercent = 12.5f;
+
+    int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+    int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+
+    void* MCPHandle = Mcp2221_OpenByIndex(VID, PID, 0);
+
+    if (reinterpret_cast<std::size_t>(MCPHandle) == -1) {
+        std::cout << "Could not connect to MCP!";
+        return -1;
+    }
+
+    std::cout << reinterpret_cast<std::size_t>(MCPHandle);
+
+    Mcp2221_SetSpeed(MCPHandle, I2CSpeed);
+
+    HSYNTHETICPOINTERDEVICE synthPointer = CreateSyntheticPointerDevice(PT_PEN, 1, POINTER_FEEDBACK_DEFAULT);
+
+    coords.x = 500;
+    coords.y = 500;
+    SetCursorPos(coords.x, coords.y);
+    pointerInfo.pointerType = PT_PEN;
+    pointerInfo.pointerId = 0;
+    pointerInfo.pointerFlags = POINTER_FLAG_INCONTACT | POINTER_FLAG_PRIMARY;
+    pointerInfo.ptPixelLocation = coords;
+
+    penInfo.penFlags = PEN_FLAG_NONE;
+    penInfo.penMask = PEN_MASK_PRESSURE;
+    penInfo.pressure = 512;
+    penInfo.pointerInfo = pointerInfo;
+
+    pointerTypeInfo.penInfo = penInfo;
+    pointerTypeInfo.type = PT_PEN;
+
+
+    while (true) {
+
+        int err = Mcp2221_I2cRead(MCPHandle, NO_POS_BYTES, AR1000Address, 1, i2cPosData);
+        err = Mcp2221_I2cRead(MCPHandle, NO_PRESS_BYTES, FMAAddress, 1, i2cPressData);
+
+        int y = (int)(i2cPosData[1] | (i2cPosData[2] << 7)); //refer to the AR1000 doc, table 7.1
+        int x = (int)(i2cPosData[3] | (i2cPosData[4] << 7)); //warning, x/y are swapped here!
+
+        int force = (int)((i2cPressData[0] << 7) | i2cPressData[1]);
+
+
+        double xr = 1.0f - (x / (double)xMax); //because I want it mirrored
+        double yr = y / (double)yMax;
+        
+        if(x != 0 && y != 0){
+            std::cout << xr << '\t' << yr << '\n';
+            int xCursor = (int)((xr * screenWidth * (100 + edgePercent) / 100.0f - edgePercent * 0.5f * screenWidth / 100.0f));
+            int yCursor = (int)((yr * screenHeight * (100 + edgePercent) / 100.0f - edgePercent * 0.5f * screenHeight / 100.0f));
+
+            updatePen(xCursor, yCursor, force);
+            updatePointerFlag(POINTER_FLAG_INRANGE | POINTER_FLAG_PRIMARY);
+            InjectSyntheticPointerInput(synthPointer, &pointerTypeInfo, 1);
+        }
+        else {
+            updatePointerFlag(POINTER_FLAG_UP);
+        }
+        
+        
+
+        Sleep(1);
+    }
+    std::cout << "Hello World!\n";
+}
+
